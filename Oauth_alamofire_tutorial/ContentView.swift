7fefd7dad90e702b1dd71f67d16624faf8f083f5
hyeju-kim <gpwn243@naver.com>
2023-03-03 01:27:13 +0900

    Initial Commit

---
+++ Oauth_alamofire_tutorial/ContentView.swift
@@ -0,0 +1,77 @@
+//
+//  ContentView.swift
+//  Oauth_alamofire_tutorial
+//
+//  Created by 김혜주 on 2023/03/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView{
+            VStack(spacing: 0){
+                Image(systemName: "lock.circle")
+                    .font(.system(size: 160)) //아이콘 크기
+                Spacer()
+                NavigationLink(destination: Text("로그인 화면"), label: {
+                    HStack{
+                        Spacer()
+                        Text("로그인 하러가기")
+                        Spacer()
+                    }
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 6))
+                }) //destination: 넘어가는 화면, label: 실제 보여주는 부분
+                .padding([.bottom], 10)
+                NavigationLink(destination: Text("회원가입 화면"), label: {
+                    HStack{
+                        Spacer()
+                        Text("회원가입 하러가기")
+                        Spacer()
+                    }
+                    .padding()
+                    .background(Color.black)
+                    .foregroundColor(.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 6))
+                })
+                
+                Spacer().frame(height: 40) //간격두기
+                NavigationLink(destination: Text("내 프로필"), label: {
+                    HStack{
+                        Spacer()
+                        Text("내 프로필 보러가기")
+                        Spacer()
+                    }
+                    .padding()
+                    .background(Color.yellow)
+                    .foregroundColor(.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 6))
+                })
+                .padding([.bottom], 10) //아래 간격
+                NavigationLink(destination: Text("사용자 목록"), label: {
+                    HStack{
+                        Spacer()
+                        Text("사용자 목록 보러가기")
+                        Spacer()
+                    }
+                    .padding()
+                    .background(Color.green)
+                    .foregroundColor(.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 6))
+                })
+                Spacer()
+                Spacer() //간격 맞추기
+        }//VStack
+            .padding() //화면에 맞춰짐!
+        }//NavigationView
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Oauth_alamofire_tutorial/Oauth_alamofire_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Oauth_alamofire_tutorialApp.swift
+//  Oauth_alamofire_tutorial
+//
+//  Created by 김혜주 on 2023/03/03.
+//
+
+import SwiftUI
+
+@main
+struct Oauth_alamofire_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
